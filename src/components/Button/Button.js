3fa5460e4d7438d59a20fcc3d4f022971b399738
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-07-16 16:31:32 -0400

    Add Default, standardize hover, outline, haze, large, large outline, large haze, and small all

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,43 +1,49 @@
 import React from "react";
 import "./Button.css";
 
-// This is a functional component - just set up a little differently as an arrow function!
 const Button = (props) => {
   // Declare a classList variable and set it to an empty string
   let classList = "";
 
-  // Create an array of all of the story/component types you want to be
-  // included in your component library
+  // Array of all story/component types in the component library
+  // Default cases have no type attribute
   let types = ["primary", "danger", "success", "warning"];
 
-  // Add a conditional statement that checks for the type and updates the
-  // classList variable based on their existence.
+  // Check if type is true and
+  // Update classList variable if not default
   if (types.includes(props.type)) {
     classList += ` button-${props.type}`;
   }
 
-  // Add other conditional statement to check for additional properties (such as large)
-  // and add to the classList variable based on this condition evaluating to true
+  // Check if large, small, outline, and/or haze props are true
+  // Update classList variable
   if (props.large) {
-    classList += ` button-large`; // Note the spacing here since we are adding to the string!
+    classList += ` button-large`; // Note initial empty space as we add to the string
   }
 
   if (props.small) {
-    classList += ` button-small`; // Note the spacing here since we are adding to the string!
-  }
-
-  // Add a conditional statement that checks for hover and outline and updates the
-  // classList variable based on their existence. Use ${props.type} for this to work on multiple types
-  if (props.hover) {
-    classList += ` button-${props.type}:hover`; // Note the spacing here since we are adding to the string!
+    classList += ` button-small`; // Note initial empty space as we add to the string
   }
 
   if (props.outline) {
-    classList += ` button-${props.type}-outline`; // Note the spacing here since we are adding to the string!
+    classList += ` button-outline`; // Note initial empty space as we add to the string
   }
 
-  // Give the button's class a value of classList
-  return <button className={classList} onClick=props.whenClicked() >{props.label}</button>;
+  if (props.haze) {
+    classList += ` button-haze`; // Note initial empty space as we add to the string
+  }
+
+  // Add a conditional statement that checks for hover and updates the
+  // classList variable based on their existence. Use ${props.type} for this to work on multiple types
+  //   if (props.hover) {
+  //     classList += ` hover`; // Note the spacing here since we are adding to the string!
+  //   }
+
+  // Button class needs a value of classList,
+  // to which we add the classList string
+  return <button className={classList}>{props.label}</button>;
 };
 
 export default Button;
+
+// onClick=props.whenClicked()
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,22 +1,55 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-
 import Button from "./Button";
 
 storiesOf("Button", module)
-  .add("Primary", () => <Button type="primary" label="Primary" />)
+  .add("Default", () => <Button label="Do something" />)
+  .add("Default Hover", () => <Button hover label="Do something" />)
+  .add("Default Outline", () => <Button outline label="Do something" />)
+  .add("Default Haze", () => <Button haze label="Do something" />)
+  .add("Default Large", () => <Button large label="Do something" />)
+  .add("Default Large Outline", () => (
+    <Button large outline label="Do something" />
+  ))
+  .add("Default Large Haze", () => <Button large haze label="Do something" />)
+  .add("Default Small", () => <Button small label="Do something" />)
+  .add("Default Small Outline", () => (
+    <Button small outline label="Do something" />
+  ))
+  .add("Default Small Haze", () => <Button small haze label="Do something" />)
+
+  .add("Primary", () => <Button type="primary" label="Do something" />)
   .add("Primary Hover", () => (
-    <Button type="primary" hover label="Primary Hover" />
+    <Button type="primary" hover label="Do something" />
   ))
   .add("Primary Outline", () => (
-    <Button type="primary" outline label="Primary Outline" />
+    <Button type="primary" outline label="Do something" />
   ))
-  .add("Danger", () => <Button type="danger" label="Danger" />)
-  .add("Success", () => <Button type="success" label="Success" />)
-  .add("Warning", () => <Button type="warning" label="Warning" />)
-  .add("Large Primary", () => (
-    <Button type="primary" large label="Large Primary Button" />
+  .add("Primary Haze", () => (
+    <Button type="primary" haze label="Do something" />
   ))
-  .add("Small Primary", () => (
-    <Button type="primary" small icons="heart" label="Small Primary Button" />
-  ));
+  .add("Primary Large", () => (
+    <Button type="primary" large label="Do something" />
+  ))
+  .add("Primary Large Outline", () => (
+    <Button type="primary" large outline label="Do something" />
+  ))
+  .add("Primary Large Haze", () => (
+    <Button type="primary" large haze label="Do something" />
+  ))
+  .add("Primary Small", () => (
+    <Button type="primary" small label="Do something" />
+  ))
+  .add("Primary Small Outline", () => (
+    <Button type="primary" small outline label="Do something" />
+  ))
+
+  .add("Primary Small Haze", () => (
+    <Button type="primary" small haze label="Do something" />
+  ))
+
+  .add("Danger", () => <Button type="danger" label="Do something" />)
+  .add("Success", () => <Button type="success" label="Do something" />)
+  .add("Warning", () => <Button type="warning" label="Do something" />);
+
+//   icons="heart"
