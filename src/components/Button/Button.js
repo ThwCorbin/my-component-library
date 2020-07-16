d39cb90c647f0335c7e4eeb1284c173890ff154e
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-07-16 12:52:23 -0400

    Initial project commit

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React from "react";
+import "./Button.css";
 
-// This is a functional component - just sent up a little differently as an arrow function!
-const Button = (props) => (
-    <button>
-        {props.label}
-    </button>
-)
+// This is a functional component - just set up a little differently as an arrow function!
+const Button = (props) => {
+  // Declare a classList variable and set it to an empty string
+  let classList = "";
 
-export default Button; 
+  // Create an array of all of the story/component types you want to be
+  // included in your component library
+  let types = ["primary", "danger", "success", "warning"];
+
+  // Add a conditional statement that checks for the type and updates the
+  // classList variable based on their existence.
+  if (types.includes(props.type)) {
+    classList += ` button-${props.type}`;
+  }
+
+  // Add other conditional statement to check for additional properties (such as large)
+  // and add to the classList variable based on this condition evaluating to true
+  if (props.large) {
+    classList += ` button-large`; // Note the spacing here since we are adding to the string!
+  }
+
+  if (props.small) {
+    classList += ` button-small`; // Note the spacing here since we are adding to the string!
+  }
+
+  // Add a conditional statement that checks for hover and outline and updates the
+  // classList variable based on their existence. Use ${props.type} for this to work on multiple types
+  if (props.hover) {
+    classList += ` button-${props.type}:hover`; // Note the spacing here since we are adding to the string!
+  }
+
+  if (props.outline) {
+    classList += ` button-${props.type}-outline`; // Note the spacing here since we are adding to the string!
+  }
+
+  // Give the button's class a value of classList
+  return <button className={classList} onClick=props.whenClicked() >{props.label}</button>;
+};
+
+export default Button;
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import { storiesOf } from "@storybook/react";
 
-import Button from './Button';
+import Button from "./Button";
 
-storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+storiesOf("Button", module)
+  .add("Primary", () => <Button type="primary" label="Primary" />)
+  .add("Primary Hover", () => (
+    <Button type="primary" hover label="Primary Hover" />
+  ))
+  .add("Primary Outline", () => (
+    <Button type="primary" outline label="Primary Outline" />
+  ))
+  .add("Danger", () => <Button type="danger" label="Danger" />)
+  .add("Success", () => <Button type="success" label="Success" />)
+  .add("Warning", () => <Button type="warning" label="Warning" />)
+  .add("Large Primary", () => (
+    <Button type="primary" large label="Large Primary Button" />
+  ))
+  .add("Small Primary", () => (
+    <Button type="primary" small icons="heart" label="Small Primary Button" />
+  ));
